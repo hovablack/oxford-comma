54e6706dbc6c9807fc7e8994f1856af35a22fc5e
hovablack <nevermind92@hotmail.com>
2020-10-04 11:11:24 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
 
+
+# def oxford_comma(array)
+#     if array.length == 1
+#     array.join()
+#     elsif array.length == 2
+#         array.join(" and ")
+#     elsif array.length > 2
+#         [array[0..-2].join(", "), array[-1]].join(", and ")
+#     end
+# end
+
+def oxford_comma(array)
+    if array.size == 1
+        array.join()
+    elsif array.size == 2
+        array.join(" and ")
+    else array.size > 2
+        [array[0..-2].join(", "), array[-1]].join(", and ")
+    end
 end
